5987f1ff74b7d0fad7854aad035915f0eb61b334
feizhihui <helloworld@csu.edu.cn>
2018-01-02 18:57:36 +0800

    update focal loss

--- nn/train_this.py
+++ nn/train_this.py
@@ -10,7 +10,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 Reader = data_input.data_master()
 
 learning_rate = 0.001
-batch_size = 256  # best 256 0.90  0.98
+batch_size = 256  # best 256 0.90  0.98   lr=0.001
 epoch_num_cnn = 70
 
 keep_pro = 0.90
